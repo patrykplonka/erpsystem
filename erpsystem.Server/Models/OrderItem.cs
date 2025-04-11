12fca080a8863bc5a58c720372421d8ccd5cb627
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2025-04-11 17:36:51 +0200

    Added OrderItem model.

---
+++ erpsystem.Server/Models/OrderItem.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace erpsystem.Server.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+
+        [Required]
+        public int WarhouseItemId { get; set; } 
+        public WarehouseItem WarehouseItem { get; set; } 
+
+        [Required]
+        public string ProductName { get; set; } 
+
+        [Required]
+        public decimal Quantity { get; set; } 
+
+        [Required]
+        public decimal UnitPriceNet { get; set; } 
+
+        public decimal UnitPriceGross { get; set; } 
+
+        [Required]
+        public decimal TaxRate { get; set; } 
+
+        public decimal Discount { get; set; } 
+    }
+}
